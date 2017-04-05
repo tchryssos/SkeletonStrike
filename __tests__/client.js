590579b5138a5efcf00fc21b7b4e798c7d9c7196
Troy Chryssos <troychryssos@gmail.com>
2017-04-05 12:20:09 -0400

    kyle helped me lint everything hurray

--- __tests__/client.js
+++ __tests__/client.js
@@ -1,13 +1,13 @@
-import 'react-native';
-import 'react-vr';
-import React from 'react';
-import Index from '../index.vr.js';
+import 'react-native'
+import 'react-vr'
+import React from 'react'
+import Index from '../index.vr.js'
 
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import renderer from 'react-test-renderer'
 
 it('renders correctly', () => {
-  const tree = renderer.create(
-    <Index />
-  );
-});
+	const tree = renderer.create(
+		<Index />
+  )
+})
--- components/ThemeMusic.js
+++ components/ThemeMusic.js
@@ -3,27 +3,27 @@ import { Sound, asset } from 'react-vr'
 
 export default class ThemeMusic extends React.Component {
 
-  constructor(props) {
-    super(props)
-  }
+	constructor(props) {
+		super(props)
+	}
 
-  get volume() {
-    if (!this.props.victory) {
-      return 1
-    } else {
-      return 0
-    }
-  }
+	get volume() {
+		if (!this.props.victory) {
+			return 1
+		} else {
+			return 0
+		}
+	}
 
-  render() {
-    return (
-      <Sound
-        source={{
-          mp3: asset('spooky.mp3')
-        }}
-        loop={true}
-        volume={this.volume}
-      />
+	render() {
+		return (
+			<Sound
+				source={{
+					mp3: asset('spooky.mp3'),
+				}}
+				loop
+				volume={this.volume}
+			/>
     )
-  }
+	}
 }
